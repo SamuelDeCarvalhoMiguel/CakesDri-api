0f27a1e82376145d812ebff650ddbc45c8a93aad
Samuel de Carvalho <samuel@guardianrh.com.br>
2024-04-03 04:07:19 -0300

    Injeção de dependencia

---
+++ cakesdri-api/Interfaces/IRecipeRepository.cs
@@ -0,0 +1,13 @@
+﻿using cakesdri_api.Models;
+
+namespace cakesdri_api.Interfaces
+{
+  public interface IRecipeRepository
+  {
+    void Insert(Recipe recipe);
+    void Update(Recipe recipe);
+    void Delete(int id);
+    Task<Recipe> GetById(int id);
+    Task<IEnumerable<Recipe>> GetAll();
+  } 
+}
---
+++ cakesdri-api/Repositories/RecipeRepository.cs
@@ -0,0 +1,39 @@
+﻿using cakesdri_api.Interfaces;
+using cakesdri_api.Models;
+
+namespace cakesdri_api.Repositories
+{
+  public class RecipeRepository : IRecipeRepository
+  {
+    private readonly CakesdriContext _context;
+
+    public RecipeRepository(CakesdriContext context)
+    {
+      _context = context;
+    }
+
+    public void Insert(Recipe recipe)
+    {
+      throw new NotImplementedException();
+    }
+    public void Update(Recipe recipe)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<Recipe> GetById(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Recipe>> GetAll()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
